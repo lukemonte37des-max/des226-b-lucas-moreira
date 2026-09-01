3948fc541b3d02121c7d47ff3f670a3e21f63849
lukemonte37des-max <luke.monte37des@gmail.com>
2026-09-01 20:41:51 -0300

    atividade 006

--- 005-atividade.js
+++ 005-atividade.js
@@ -5,6 +5,13 @@ let profissao = entrada("Digite a sua profissão");
 let ano = entrada("Digite o ano que nasceu");
 
 console.log("");
+console.log("---------------------------------------");
+console.log("Nome: " + nome);
+console.log("Profissão: " + profissao);
+console.log("Ano de Nascimento" + ano);
+
+console.log("");
+console.log("---------------------------------------");
 console.log("Nome: " + typeof nome);
 console.log("Profissão: " + typeof profissao);
 console.log("Ano de Nascimento" + typeof ano);
@@ -18,3 +25,5 @@ console.log();
 nomeDigitado = entrada("Seu Nome Completo");
 cargoDigitado = entrada("Sua Profissão ou Cargo Atual");
 anoDigitado = entrada("Ano de Nascimento");
+
+entrada();
---
+++ 006-atividade.js
@@ -0,0 +1,33 @@
+let entrada = require("prompt-sync")();
+
+console.log("Digite o nome de jogo a ser avaliado:"),
+let jogo = entrada();
+console.log("Que nota você dá para esse jogo (0-10):"),
+let nota = entrada();
+console.log("Você recomendaria esse jogo? (sim/não:)"),
+let recomenda = entrada();
+
+// eu controlo o formato
+let engajamento;
+let numPaisesExibidos = null;
+let notaInt = parseInt(nota, 10);
+let recomendaBool = recomenda === "sim" ? true : false;
+let ehNulo = numPaisesExibidos === null ? "null" : "outra coisa";
+
+
+
+console.log("");
+console.log("---------------------------------------");
+console.log("engajamento: " + typeof(engajamento));
+console.log("numPaisesExibidos: " + typeof(numPaisesExibidos));
+console.log("notaInt: " + typeof(notaInt));
+// o retorno de um null é object
+console.log("recomendaBool: " + typeof(recomendaBool));
+
+
+
+
+entrada();
+
+
+
